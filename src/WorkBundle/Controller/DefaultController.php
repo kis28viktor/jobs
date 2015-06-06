04e4e82ea9f6f0c5af8c9f7ea73b303cf8306aab
kis28viktor <kis28viktor@gmail.com>
2015-06-06 21:21:25 +0300

    - implemented search by cities on the start page. - it is implemented only fields on index.html.twig and subscribed route and allIssuesAction in Default controler. No logic yet.

--- src/WorkBundle/Controller/DefaultController.php
+++ src/WorkBundle/Controller/DefaultController.php
@@ -3,11 +3,17 @@
 namespace WorkBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
         return $this->render('WorkBundle:Default:index.html.twig');
+    }
+
+    public function allIssuesAction(Request $request)
+    {
+        var_dump($request->query->all());die;
     }
 }
